83bb0e0687feb4adf543930013449a4b4e615644
premkekk <premk_ekk@yahoo.com>
2022-02-14 16:35:29 -0500

    display symbol history info provide multiple routes via textbox and via dropdown

--- app.py
+++ app.py
@@ -19,26 +19,52 @@ app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
 
 
-# new endpoint to post data
+# new endpoint to post data using textbox as input
 # route decorators - handle both GET (display) and POST (submit)
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/textbox', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         symReq = request.form
         psymbol = symReq['symbol']
-        return redirect('/symbolsdata?sym=' + psymbol)
-    return render_template('index.html')
+        return redirect('/symbolsinfo?sym=' + psymbol)
+    return render_template('textbox.html')
 
 
-#new endpoint to fetch and display data
-@app.route('/symbolsdata')
+# new endpoint to post data using dropdown as input
+# route decorators - handle both GET (display) and POST (submit)
+@app.route('/', methods=['GET', 'POST'])
+def dropdown():
+    if request.method == 'POST':
+        symReq = request.form
+        psymbol = symReq['ddSymbols']
+        return redirect('/symbolhist?sym=' + psymbol)
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT DISTINCT SYMBOL FROM symbols ORDER BY 1")
+    if resultValue > 0:
+        symdataDetails = cur.fetchall()
+        return render_template('dropdown.html', symdataDetails=symdataDetails)
+
+
+#new endpoint to fetch and display symbolsinfo
+@app.route('/symbolsinfo')
 def symbolsdata():
     psym = request.args.get('sym')
     cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM symbols WHERE symbol = '" + psym + "'")
     if resultValue > 0:
         symdataDetails = cur.fetchall()
-        return render_template('symboldetails.html', symdataDetails=symdataDetails)
+        return render_template('symbolinfo.html', symdataDetails=symdataDetails)
+
+
+#new endpoint to fetch and display historical symbol data
+@app.route('/symbolhist')
+def symbolhist():
+    psym = request.args.get('sym')
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT SYMBOL, HISTDATE, OPENPRICE, CLOSEPRICE, HIGHPRICE, LOWPRICE FROM symhistory WHERE symbol = '" + psym + "' and histdate > '2022-01-01'")
+    if resultValue > 0:
+        symdataDetails = cur.fetchall()
+        return render_template('symbolhist.html', symdataDetails=symdataDetails)
 
 
 if __name__ == '__main__':
